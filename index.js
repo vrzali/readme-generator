a3c2594c11c0dfaa943cce604241409dfb2ca6c8
Vadad Rzali <vedad.rzali@gmail.com>
2022-07-05 16:30:25 -0400

    Built a questions code and downgraded inquirer

---
+++ index.js
@@ -0,0 +1,69 @@
+
+const inquirer = require('inquirer');
+
+
+
+function promptUser() {
+    return inquirer.prompt ([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project name?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project:"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process, if applicable: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            tyoe: "list",
+            name: "lisence",
+            message: "Choose the applicable license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this project?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "What are the test instructions?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email"
+        }
+    ]);
+}
+promptUser();
